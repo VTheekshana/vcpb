be4f70df421e998c0a6d6b9a1e202ac568f4c6a1
Vinu Theekshana <87488227+VTheekshana@users.noreply.github.com>
2021-07-27 15:17:02 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, message: Message):
-     await message.reply_text("**✨ Join @InfinityJE**")
+     await message.reply_text("**Hi There👋🏻 I'am Group Music Player Made By @ImTheekshana**")
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
@@ -13,7 +13,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url="https://github.com/ImJanindu/vcpb")
+                        "🛠 Source Code 🛠", url="https://github.com/VTheekshana/vcpb")
                 ]
             ]
         )
