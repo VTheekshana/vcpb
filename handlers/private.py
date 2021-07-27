9cc16918241408592704a0c1ddd62cf29f856aa2
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-07-27 13:26:15 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+
+@Client.on_message(filters.command(start) & filters.private)
+async def start(_, message: Message):
+     await message.reply_text("**✨ Join @InfinityJE**")
+
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
       await message.reply_text("""**🔴 Group Music Player Online.**""",
