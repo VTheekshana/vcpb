77f915a956055dd8dadd265b4b7e696a874fbccc
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-07-27 13:26:30 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.command(start) & filters.private)
+@Client.on_message(filters.command("start") & filters.private)
 async def start(_, message: Message):
      await message.reply_text("**✨ Join @InfinityJE**")
 
